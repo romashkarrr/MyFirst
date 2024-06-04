54f756428cb076406109c099bf47c17a687e646c
romashkarrr <r0man.kish999@gmail.com>
2024-06-04 10:52:31 +0300

    Initial Commit

---
+++ MyFirst/MyFirstApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyFirstApp.swift
+//  MyFirst
+//
+//  Created by romashka on 04.06.2024.
+//
+
+import SwiftUI
+
+@main
+struct MyFirstApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
